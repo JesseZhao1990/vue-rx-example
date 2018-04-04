1aa77e8a74fd567611665b777b85f88b242dadde
zhaojianxin <zhaojianxin@gozap.com>
2018-04-04 15:13:00 +0800

    test

--- config/index.js
+++ config/index.js
@@ -54,7 +54,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist/',
 
     /**
      * Source Maps
